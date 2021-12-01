e84651fd6c8947a7dbf63c7d9ce71fe95c7b1a1d
fujiihc <64285740+fujiihc@users.noreply.github.com>
2021-12-01 12:13:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def home():
             return redirect(url_for('catalog'))
         elif request.form.get('Button_2'):
             return redirect(url_for('button2'))
-    return render_template('home.html')
+    return render_template('search.html')
 
 
 
