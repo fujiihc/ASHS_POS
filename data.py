bbf18e31780977e299ccd4b4f5dce5268dc1464c
fujiihc <64285740+fujiihc@users.noreply.github.com>
2022-02-22 18:17:55 -0500

    Update data.py

--- data.py
+++ data.py
@@ -3,9 +3,8 @@ import pandas as pd
 
 class data:
     def __init__(self):
-        self.df = pd.read_csv('courses.csv', encoding = 'latin1')
+        self.df = pd.read_csv('courses.csv', encoding='cp1252')
         #change encoding
         
     def getCourse(self, keyword, searchCol):
        return self.df.loc[self.df[searchCol].str.contains(keyword, na = False)]
-   
