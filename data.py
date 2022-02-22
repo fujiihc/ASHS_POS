2b77128956df9213e58b308d79fabebb6dd1b95d
fujiihc <64285740+fujiihc@users.noreply.github.com>
2022-02-22 17:26:39 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
+
 class data:
     def __init__(self):
-        self.df = pd.read_csv('22-23CourseMaster.csv')
+        self.df = pd.read_csv('courses.csv', encoding = 'latin1')
         #self.df.columns = ['Course','Short Description','Curriculumn','Len', 'Crs Typ', 'Dept Cd', 'Earned Crdts', 'Grade Req Crdts', 'Grd L', 'Grd H', 'T G']
 
     def getCourse(self, courseName):
-       return self.df.loc[self.df['Short Description'].str.contains(courseName, na = False)]
+       return self.df.loc[self.df['CourseName'].str.contains(courseName, na = False)]
    
