110ed2ccd98d90796490f9e0a6ec03867328eacf
fujiihc <64285740+fujiihc@users.noreply.github.com>
2022-02-24 10:29:01 -0500

    Update data.py

--- data.py
+++ data.py
@@ -8,11 +8,6 @@ class data:
     def getCourse(self, keywords, searchCol):
        return self.df.loc[self.searchCheck(keywords.split(' '), self.df['longDescription'].str.split(' '))]
 
-   #returns a boolean array
-   #edit to take fragments that aren't the same length as the results
-   #doing so could result in one return statement for getCourse()
-   #maybe like a rolling check?
-   #or maybe like i guess being able to include multiple keywords by including a multi keyword checker num in getCourse like keyword Num and I could run a for loop
     def searchCheck(self, keyArr, searchArr):
         finalList = []
         for item in searchArr:
@@ -27,8 +22,5 @@ class data:
             toAdd = True
             for boolean in check:
                 if boolean == False:
-                    toAdd = False
-                    
-            #have to make sure that all keywords are contained within the results and only those ones
             finalList.append(toAdd)   
         return finalList
