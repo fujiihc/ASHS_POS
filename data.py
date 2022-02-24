e22e91c47d20b3b1c64f9bb3a2bc07b2799824e5
fujiihc <64285740+fujiihc@users.noreply.github.com>
2022-02-24 08:20:31 -0500

    Update data.py

--- data.py
+++ data.py
@@ -3,7 +3,32 @@ import pandas as pd
 
 class data:
     def __init__(self):
-        self.df = pd.read_csv('courses.csv', encoding='cp1252')
+        self.df = pd.read_csv('abCourseData.csv', encoding='cp1252')
         
-    def getCourse(self, keyword, searchCol):
-       return self.df.loc[self.df[searchCol].str.contains(keyword, na = False)]
+    def getCourse(self, keywords, searchCol):
+       return self.df.loc[self.searchCheck(keywords.split(' '), self.df['longDescription'].str.split(' '))]
+
+   #returns a boolean array
+   #edit to take fragments that aren't the same length as the results
+   #doing so could result in one return statement for getCourse()
+   #maybe like a rolling check?
+   #or maybe like i guess being able to include multiple keywords by including a multi keyword checker num in getCourse like keyword Num and I could run a for loop
+    def searchCheck(self, keyArr, searchArr):
+        finalList = []
+        for item in searchArr:
+            toAdd = False
+            check = []
+            for search in keyArr:
+                for word in item:
+                    if search in word:
+                        toAdd = True
+                check.append(toAdd)
+                toAdd = False
+            toAdd = True
+            for boolean in check:
+                if boolean == False:
+                    toAdd = False
+                    
+            #have to make sure that all keywords are contained within the results and only those ones
+            finalList.append(toAdd)   
+        return finalList
