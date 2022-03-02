3e1baac6fdabe30ecf82aac074df7e18a5320489
fujiihc <64285740+fujiihc@users.noreply.github.com>
2022-03-02 16:25:11 -0500

    Update data.py

--- data.py
+++ data.py
@@ -5,21 +5,25 @@ class data:
         self.df = dataframe
         
     def findCourse(self, keywords, searchCol):
-       return data(self.df.loc[self.searchCheck(keywords.split(' '), self.df[searchCol].str.split(' '))])
+        return data(self.df.loc[self.searchCheck(keywords.split(' '), self.df[searchCol].str.split(' '))])
 
     def getDF(self):
         return self.df
 
     def searchCheck(self, keyArr, searchArr):
-        finalList = []
-        for item in searchArr:
+        finalList = []        
+        for item in searchArr:            
             toAdd = False
             checkArr = []
             for search in keyArr:
-                for word in item:
-                    if search in word:
-                        toAdd = True
-                        break
+                #gotta make sure that item is not nan
+                #run an isinstance to make sure its not a float or something
+                if not isinstance(item, float):
+                    for word in item:
+                        if search in word:
+                            toAdd = True
+                            break
+                #remember that this indent could cause issues
                 checkArr.append(toAdd)
                 toAdd = False
             toAdd = True
