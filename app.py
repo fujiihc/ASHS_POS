b573e356ee284071547b5aa38129e4743029e837
fujiihc <64285740+fujiihc@users.noreply.github.com>
2022-03-07 20:51:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
-
+#requirements removed: Flask~=1.1
 import data as dt
 import pandas as pd
 from flask import Flask, render_template, redirect, url_for, request
